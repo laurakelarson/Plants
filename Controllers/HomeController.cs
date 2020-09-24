25646673dfbbbd6fe0e5a83d53d6a5ef7964e532
Laura Larson <larsonlaura@seattleu.edu>
2020-09-24 14:00:27 -0700

    Added PlantsIndexController and migrated PlantsIndex

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,11 +20,6 @@ namespace Plants.Controllers
             return View();
         }
 
-        public IActionResult PlantsIndex()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
