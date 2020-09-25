f2c283b48542d12e854d8b5332b8fac35856c066
Laura Larson <larsonlaura@seattleu.edu>
2020-09-24 22:50:35 -0700

    Added ErrorViewModelTests class and got to 100% coverage of Models

---
+++ UnitTestPlants/ModelTests/ErrorViewModelTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Plants.Models;
+
+namespace UnitTestPlants
+{
+    [TestClass]
+    public class ErrorViewModelTests
+    {
+        [TestMethod]
+        public void SetRequestIdShouldPass()
+        {
+            ErrorViewModel evm = new ErrorViewModel();
+            evm.RequestId = "BOGUS";
+
+            Assert.AreEqual(evm.RequestId, "BOGUS");
+        }
+
+        [TestMethod]
+        public void GetRequestIdShouldPass()
+        {
+            ErrorViewModel evm = new ErrorViewModel
+            {
+                RequestId = "BOGUS"
+            };
+            var res = evm.RequestId;
+
+            Assert.AreEqual(res, "BOGUS");
+        }
+
+        [TestMethod]
+        public void ShowRequestIdShouldPass()
+        {
+            ErrorViewModel evm = new ErrorViewModel
+            {
+                RequestId = "BOGUS"
+            };
+            var res = evm.ShowRequestId;
+
+            Assert.IsTrue(res);
+        }
+    }
+}
