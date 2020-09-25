08b8bd2d239e0d73b4dea615bb976b9435641519
Laura Larson <larsonlaura@seattleu.edu>
2020-09-24 23:13:22 -0700

    Added PlantsIndexControllerTests class and got overall coverage to 83%

---
+++ UnitTestPlants/ControllerTests/PlantsIndexControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Plants.Controllers;
+
+namespace UnitTestPlants
+{
+    [TestClass]
+    public class PlantsIndexControllerTests
+    {
+        [TestMethod]
+        public void HomeControllerIndexShouldPass()
+        {
+            PlantsIndexController hc = new PlantsIndexController();
+            var res = hc.PlantsIndex();
+
+            Assert.IsNotNull(res);
+        }
+    }
+}
