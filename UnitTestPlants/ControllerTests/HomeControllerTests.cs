5876c7d677d2815d79d04adcd79fc3dee7c10f79
Laura Larson <larsonlaura@seattleu.edu>
2020-09-24 23:10:05 -0700

    Added HomeControllerTests and got controllers coverage to 27%

---
+++ UnitTestPlants/ControllerTests/HomeControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Plants.Controllers;
+
+namespace UnitTestPlants
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void HomeControllerIndexShouldPass()
+        {
+            HomeController hc = new HomeController();
+            var res = hc.Index();
+
+            Assert.IsNotNull(res);
+        }
+
+        [TestMethod]
+        public void HomeControllerAboutUsShouldPass()
+        {
+            HomeController hc = new HomeController();
+            var res = hc.AboutUs();
+
+            Assert.IsNotNull(res);
+        }
+
+    }
+}
