933555b9323f48f5e701fe1ccd04383199a72ff7
Laura Larson <larsonlaura@seattleu.edu>
2020-09-24 17:32:07 -0700

    Changed folder structure to try and add UT to source control

---
+++ UnitTestPlants/PlantModelTests.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Plants.Models;
+
+namespace UnitTestPlants
+{
+    [TestClass]
+    public class PlantModelTests
+    {
+        [TestMethod]
+        public void TestDefaultConstructor()
+        {
+            PlantModel plantTest = new PlantModel();
+            Assert.IsTrue(true);
+        }
+
+        public void TestDefaultConstructor2()
+        {
+            PlantModel plantTest = new PlantModel();
+            Assert.IsTrue(true);
+        }
+
+        public void TestDefaultConstructor3()
+        {
+            PlantModel plantTest = new PlantModel();
+            Assert.IsTrue(true);
+        }
+    }
+}
