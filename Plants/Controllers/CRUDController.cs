3bac19a8ad3d7da9624371365479d7b3f4225fc7
Laura Larson <larsonlaura@seattleu.edu>
2020-09-30 15:20:36 -0700

    Added CreatePlant view and CRUD controller

---
+++ Plants/Controllers/CRUDController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Plants.Controllers
+{
+    public class CRUDController : Controller
+    {
+        public IActionResult CreatePlant()
+        {
+            return View();
+        }
+    }
+}
