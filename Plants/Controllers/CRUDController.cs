5206d654f3866187150ce1e0319b98e690a7c6a2
Laura Larson <larsonlaura@seattleu.edu>
2020-09-30 15:43:54 -0700

    Added DeletePlant to views

--- Plants/Controllers/CRUDController.cs
+++ Plants/Controllers/CRUDController.cs
@@ -22,5 +22,10 @@ namespace Plants.Controllers
         {
             return View();
         }
+
+        public IActionResult DeletePlant()
+        {
+            return View();
+        }
     }
 }
